e4b4ed596b41b53bef003edd09f54567fc4e3958
vallemkr <vallemkr@umich.edu>
2020-12-02 16:25:10 -0500

    map, got multiple fights in same location working, and colors

--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -13,13 +13,34 @@ export default {
  data() {
    return{
      center: [42.290928, -83.717708], // Pierpont Commons
-     currentFightLocations: [
-// Hard code some locations into create event to make for easier and more legal location selection
 
-       [42.292149,-83.715727], // North Campus Grove
-        [42.289078,-83.709831], // Arborcrest Cemetery
-        [42.280617,-83.732652], // Palmer Field
+      // Hard code some locations into create event to make for easier and more legal location selection
+     fights: [
+        {
+         team1: "Alphas",
+         team2: "Omegas",
+         location: "North Campus Grove", 
+         time: "9:00 PM"
+        },
+        {
+         team1: "Surfers",
+         team2: "Bikers",
+         location: "North Campus Grove", 
+         time: "3:00 PM"
+        },
+        {
+         team1: "Couzens",
+         team2: "Alice",
+         location: "Palmer Field", 
+         time: "10:00 AM"
+        },
      ],
+
+     coordinates: {
+       "Palmer Field": [42.280617,-83.732652],
+       "North Campus Grove": [42.292149,-83.715727],
+       "Arborcrest Cemetery": [42.289078,-83.709831],
+     },
    }
  },
 
@@ -42,13 +63,71 @@ export default {
 
 
       //## add current fight location markers
-      for (let fightLocation of this.currentFightLocations) {
-        L.marker(fightLocation)
-          .bindPopup("Hi!")
-          .addTo(mapDiv);
-        console.log(fightLocation);
+
+      // the message to display when each location marker is clicked (list of all fights at that location)
+      const locationPopupMsgs = {};
+
+      // create the popup messages for each location
+      for (let fight of this.fights) {
+        let { location } = fight;
+        
+        // initialize "" if location not already existing
+        if (! (location in locationPopupMsgs)) {
+          locationPopupMsgs[location] = "";
+        }
+
+        locationPopupMsgs[location] += `${fight.team1} vs ${fight.team2}<br>${fight.time}<br><br>`;
+      }
+
+      //add markers with their correponding popup messages to map
+      for (let location in locationPopupMsgs) {
+        let msg = locationPopupMsgs[location];
+
+        console.log(msg);
+
+        let coordinates = this.coordinates[location];
+
+// icon colors:
+// https://github.com/pointhi/leaflet-color-markers
+        var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+        
+        L.marker(coordinates, {icon: greenIcon})
+          .bindPopup(msg)
+          .addTo(mapDiv)
+          ;
       }
    },
+
+   isThereAFightHereRightNow: function(location) {
+
+     for (const fight of this.fights) {
+       const fightTime = fight.time;
+
+       const currentTime = (new Date());
+
+       // todo
+
+       /*
+
+       if fightTime <= currentTime && currentTime <= fightTime:
+          reuturn True
+        else:
+          return False
+
+Blue markers by default, yellow for ongoing fight
+          */
+     }
+
+     // return True if theres a current fight
+     // False otherwise
+   }
  },
 
  mounted() {
