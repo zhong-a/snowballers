a73ef4ab54e8a0daf5a29ca92873dec14286e374
Nick Ceccio <ceccion@umich.edu>
2020-12-05 09:20:10 -0500

    finished with autoteamremoval

--- src/store.js
+++ src/store.js
@@ -87,11 +87,44 @@ export default new Vuex.Store({
             const userProfile = await fb.usersCollection.doc(user.uid).get();
 
             let data = userProfile.data()
-            data['uid'] = user.uid
-            // set user profile in state
-            commit("setUserProfile", data);
 
+            //check for auto team removal
+            if (data.hasOwnProperty('inteam')) {
+                let teamid = data.inteam;
+                const team = await fb.teamsCollection.doc(teamid).get();
+                let teamData = team.data();
+                if (teamData.hasOwnProperty('fight')) {
+                    let fightid = teamData.fight;
+                    const fight = fb.fightsCollection.doc(fightid).get();
+                    let fightData = fight.data();
+
+                    let time = fightData.time;
+                    const millisecondsInAnHour = 3600000;
+                    if (Date.now() < (time + millisecondsInAnHour)) {
+                        //The fight has finished
+                        //Update the user data
+                        delete data.inteam;
+                        delete data.owner;
+                        await fb.usersCollection.doc(user.uid).set(data);
+
+                        //update the team data
+                        delete teamdata.members[user.uid]
+                        teamdata.currentMembers = teamdata.currentMembers - 1;
+                        if (teamdata.currentMembers === 0) {
+                            //the team is now empty, remove it
+                            await fb.teamsCollection.doc(teamid).delete();
+                        } else {
+                            await fb.teamsCollection.doc(teamid).set(teamdata);
+                        }
+                    } else {
+                        alert("What are you doing here? You should be in your snowball fight!")
+                    }
+                }
+            }
             
+            // set user profile in state
+            data['uid'] = user.uid
+            commit("setUserProfile", data);
             // change route to dashboard
             //router.push("/");
         },
